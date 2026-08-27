c874373f2682a6dde854a5a83506e90272a708db
ramcharan <ramcharanreddy027@gmail.com>
2026-08-27 22:47:04 +0530

    Fix product image path

--- src/js/shop.js
+++ src/js/shop.js
@@ -228,7 +228,7 @@ document.addEventListener("DOMContentLoaded", () => {
             // Images are stored inside /products/
 
             image.src =
-                '/images/products / ${ product.image }'
+                '/images/products/${ product.image }'
 
             image.alt =
                 product.name;
